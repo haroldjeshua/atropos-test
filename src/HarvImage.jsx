b379bfc25ec50221f374e5f2f2b42b0d4a9780e8
Harold Varde <harvardii15@gmail.com>
2022-11-17 22:45:16 +0800

    added offset logo

--- src/HarvImage.jsx
+++ src/HarvImage.jsx
@@ -7,6 +7,9 @@ import layer2 from "./assets/layer2.svg";
 import layer3 from "./assets/layer3.svg";
 import layer4 from "./assets/layer4.svg";
 import layer5 from "./assets/layer5.svg";
+import layerh from "./assets/layerh.svg";
+import layerj from "./assets/layerj.svg";
+import layerv from "./assets/layerv.svg";
 
 const HarvImage = () => {
   return (
@@ -18,21 +21,13 @@ const HarvImage = () => {
       >
         <img className="atropos-banner-spacer" src={layer0} alt="layer" />
         <img data-atropos-offset="-4.5" src={layer0} alt="layer" />
-        {/* <img
-          className="atropos-banner-spacer"
-          src="https://raw.githubusercontent.com/nolimits4web/atropos/master/playground/react/i/atropos-bg.svg"
-          alt="layer1"
-        />
-        <img
-          data-atropos-offset="-4.5"
-          src="https://raw.githubusercontent.com/nolimits4web/atropos/master/playground/react/i/atropos-bg.svg"
-          alt=""
-        /> */}
         <img data-atropos-offset="-2.5" src={layer1} alt="" />
         <img data-atropos-offset="0" src={layer2} alt="" />
         <img data-atropos-offset="2" src={layer3} alt="" />
         <img data-atropos-offset="4" src={layer4} alt="" />
-        <img data-atropos-offset="5" src={layer5} alt="" />
+        <img data-atropos-offset="6" src={layerh} alt="" />
+        <img data-atropos-offset="8" src={layerj} alt="" />
+        <img data-atropos-offset="10" src={layerv} alt="" />
       </Atropos>
     </div>
   );
